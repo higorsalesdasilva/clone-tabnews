90b08d490de97406e610cb8a6ceec10cb6e55fd4
Higor Sales <161096993+higorsalesdasilva@users.noreply.github.com>
2025-11-28 01:09:53 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Luana te amo, e um dia irei dar orgulho aos meus pais e a você!</h1>
+    return <h1>Luana te amo, e um dia irei dar orgulho aos meus pais e a você!!!</h1>
 }
 
 export default Home;
