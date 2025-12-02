b6d431f7be9b3396c8ae65898b79ca5c52b0d383
Higor Sales <161096993+higorsalesdasilva@users.noreply.github.com>
2025-12-02 01:19:02 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-    return <h1>Luana te amo, e um dia irei dar orgulho aos meus pais e a você!!!</h1>
+  return (
+    <h1>Luana te amo, e um dia irei dar orgulho aos meus pais e a você!!!</h1>
+  );
+}
+
+function teste1() {
+  console.log("identação errada");
 }
 
 export default Home;
